87543662b0933aac01bdff9c8078051c26a91c64
Preston West <preston.west@cyxtera.com>
2019-12-17 16:33:16 -0600

    Added morgan logging

--- make-server.js
+++ make-server.js
@@ -1,9 +1,13 @@
+const morgan = require('morgan');
+
 function makeServer(middleware) {
   const express = require('express');
   const app = express();
   const port = 3000;
 
+  app.use(morgan('tiny'));
   app.use(express.json());
+
   for (let tuple of middleware) {
     app.use(...tuple);
   }
