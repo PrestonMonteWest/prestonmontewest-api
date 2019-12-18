d36d7ab84eb09307808fd31ff7b929c0d6e1ba4e
Preston West <preston.west@cyxtera.com>
2019-12-18 12:03:23 -0600

    Added error handling for missing env vars

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
+
+if (!process.env.AWS_ACCESS_KEY_ID) {
+  throw new Error('access key Id required');
+}
+
+if (!process.env.AWS_SECRET_ACCESS_KEY) {
+  throw new Error('secret access key required');
+}
+
+if (!process.env.AWS_REGION) {
+  throw new Error('region required');
+}
+
 const postRouter = require('./routes/post');
 require('./make-server')([
   ['/post', postRouter]
